f7327e3e62c1b165a143461754b2a8daf0aa6033
Stefan Waldner <waldner.st@gmail.com>
2017-05-07 00:13:17 +0200

    kara testing implementiert

--- src/pages/home/home-data.spec.ts
+++ src/pages/home/home-data.spec.ts
@@ -1,10 +1,13 @@
 import { data } from './home-data';
 
-describe('Home data', () => {
-	it('should contain valid data for the home page', () => {
+describe('Home page', () => {
+	it('should contain a button that navigate to te following facebook page', () => {
 		expect(data.facebook).toBe('https://www.facebook.com/leoneacademy/');
 	}),
-  it('should contain a valid phone number', () => {
+  it('should contain a button with a valid phone number', () => {
     expect(data.phoneNumber).toBe('+41792007397');
   });
 });
+
+//behavior driven framwork ..
+//BDD TEST..
